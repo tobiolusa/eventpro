3be3a0a045e403655fe7bff7b5635a7cbb99fd1a
tobiolusa <olusamiracle@gmail.com>
2023-06-01 02:14:31 +0100

    Settings

--- authapi/urls.py
+++ authapi/urls.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
-from django.urls import path,include, re_path
+from django.urls import path,include, path
+from rest_framework import routers
+from authapi import views
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, login URLs included for the browsable API.
+urlpatterns = [
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+]
+
+
 
     
